80aa2b0f269224acb89c2f676d59cd9406ba7377
Hemmerson Luis Barros da Rosa <hemmerson.rosa@gmail.com>
2023-11-26 01:55:11 -0300

    first commit

---
+++ src/main/java/aula/introducaodao/Controle/Editar.java
@@ -0,0 +1,53 @@
+package aula.introducaodao.Controle;
+
+import aula.introducaodao.DAO.ErroDAO;
+import aula.introducaodao.DAO.PessoaDAOInterface;
+import aula.introducaodao.DAO.PessoaDaoJdbc;
+import aula.introducaodao.DAO.PessoaDaoXml;
+import aula.introducaodao.Modelo.Pessoa;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet("/editar")
+public class Editar extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
+        PrintWriter out = response.getWriter();
+        int id = Integer.parseInt(request.getParameter("id"));
+        String nome = request.getParameter("nome");
+        String tidade = request.getParameter("idade");
+        String tipo = request.getParameter("tipo");
+        if (tipo == null) tipo = "banco";
+        String caminho = getServletContext().getRealPath("/WEB-INF/Pessoa.xml");
+        if (nome != null && !nome.isBlank() && tidade != null && !tidade.isBlank()) {
+            Pessoa p = new Pessoa(id, nome, Integer.parseInt(tidade));
+            try {
+                PessoaDAOInterface dao;
+                if (tipo.equals("banco"))
+                    dao = new PessoaDaoJdbc();
+                else
+                    dao = new PessoaDaoXml(caminho);
+                dao.editar(p);
+                //response.getWriter().println(p);
+                out.println("Editado com sucesso.");
+                dao.close();
+            } catch (ErroDAO e) {
+                Logger.getLogger(Editar.class.getName()).log(Level.SEVERE, "Erro ao editar", e);
+                out.println("Erro ao tentar editar");
+            }
+
+        } else
+            out.println("Informe o nome e a idade");
+    }
+}
---
+++ src/main/webapp/codigo.js
@@ -0,0 +1,110 @@
+function ajax(recurso,dados,funcao,metodo)
+{
+    const xhr=new XMLHttpRequest();
+    xhr.onreadystatechange=funcao;
+    if(metodo.toLowerCase()==="get") {
+        recurso += "?"+ dados;
+        xhr.open(metodo, recurso);
+        xhr.send();
+    }else if(metodo.toLowerCase()==="post")
+    {
+        xhr.open(metodo, recurso);
+        xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+        xhr.send(dados);
+    }
+}
+onload=()=>
+{
+    limpaCampos();
+    buscaPessoas();
+}
+function buscaPessoas(){
+    ajax("buscar",`tipo=${document.querySelector("input:checked").value}`,function (){
+        if(this.readyState==4 && this.status==200)
+        {
+            mostrarDados(this.responseXML)
+        }
+    },"get");
+}
+function mostrarDados(doc)
+{
+    const pessoas=doc.documentElement.getElementsByTagName("pessoa");
+    let texto="";
+    for(let p of pessoas)
+    {
+
+        const id=p.getElementsByTagName("id")[0].textContent;
+        const nome = p.getElementsByTagName("nome")[0].textContent
+        const idade = p.getElementsByTagName("idade")[0].textContent
+
+        texto+=`<tr>
+                <td>${id}</td>
+                <td>${nome}</td>
+                <td>${idade}</td>
+                <td><button onclick="preencheEditar(${id}, '${nome}', ${idade})">Editar</button></td>
+                <td><button onclick="deletar(${id})">Deletar</button></td>
+           </tr>`;
+    }
+    document.querySelector("tbody").innerHTML=texto;
+}
+
+function limpaCampos(){
+    document.getElementById("id").value = "";
+    document.getElementById("nome").value = "";
+    document.getElementById("idade").value = "";
+    document.getElementById("btCadastrar").value = "Cadastrar";
+    document.getElementById("btCadastrar").onclick=(evento)=>{
+        evento.preventDefault();
+        const nome=document.getElementById("nome").value;
+        const idade=document.getElementById("idade").value;
+        const tipo=document.querySelector("input:checked").value;
+        dados=`nome=${nome}&idade=${idade}&tipo=${tipo}`;
+        ajax("inserir",dados,function (){
+            if(this.readyState==4 && this.status==200)
+            {
+                buscaPessoas();
+                alert(this.responseText);
+            }
+        },"post");
+        limpaCampos();
+    }
+}
+
+function deletar(id){
+    const tipo=document.querySelector("input:checked").value;
+    dados=`id=${id}&tipo=${tipo}`;
+    ajax("deletar",dados,function (){
+        if(this.readyState==4 && this.status==200)
+        {
+            buscaPessoas();
+            alert(this.responseText);
+        }
+    },"get");
+}
+
+function preencheEditar(id,nome,idade){
+    document.getElementById('id').value = id;
+    document.getElementById('nome').value = nome;
+    document.getElementById('idade').value = idade;
+    document.getElementById("btCadastrar").value = "Editar";
+    editar();
+}
+
+function editar(){
+    document.getElementById("btCadastrar").onclick=(evento)=>{
+        evento.preventDefault();
+        const id = document.getElementById("id").value;
+        const nome=document.getElementById("nome").value;
+        const idade=document.getElementById("idade").value;
+        const tipo=document.querySelector("input:checked").value;
+        dados=`id=${id}&nome=${nome}&idade=${idade}&tipo=${tipo}`;
+        ajax("editar",dados,function (){
+            if(this.readyState==4 && this.status==200)
+            {
+                buscaPessoas();
+                alert(this.responseText);
+            }
+        },"post");
+        limpaCampos();
+    }
+}
